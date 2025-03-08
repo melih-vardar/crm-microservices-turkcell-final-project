1d3457cef2c6eccda75f9a3c42ad1a75164986b1
HaticeAksuu <haticeaksu4147@gmail.com>
2025-03-09 00:07:56 +0300

    Billing-Service updated

--- billing-service/src/main/java/com/turkcell/billingservice/BillingServiceApplication.java
+++ billing-service/src/main/java/com/turkcell/billingservice/BillingServiceApplication.java
@@ -2,10 +2,14 @@ package com.turkcell.billingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EntityScan(basePackages = "com.turkcell.billingservice.entities")
+@EnableJpaRepositories(basePackages = "com.turkcell.billingservice.repositories")
 public class BillingServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(BillingServiceApplication.class, args);
