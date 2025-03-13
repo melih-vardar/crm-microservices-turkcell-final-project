0e21a7d4cc16529e32c4c57f0541b3edd8f6855a
oğuz alp çepni <oguzalpcepni@gmail.com>
2025-03-13 13:55:09 +0300

    analytics base pipeline created

---
+++ analytics-service/src/main/java/com/turkcell/analyticsservice/kafka/AnalyticsPipeline.java
@@ -0,0 +1,10 @@
+package com.turkcell.analyticsservice.kafka;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AnalyticsPipeline {
+
+
+
+}
---
+++ analytics-service/src/main/java/com/turkcell/analyticsservice/service/impl/AnalyticsServiceImpl.java
@@ -0,0 +1,8 @@
+package com.turkcell.analyticsservice.service.impl;
+
+import com.turkcell.analyticsservice.service.AnalyticsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AnalyticsServiceImpl implements AnalyticsService {
+}
