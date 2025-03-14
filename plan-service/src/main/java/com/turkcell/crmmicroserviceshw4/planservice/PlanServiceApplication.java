72173ee09877b277f2cc5479fbe33a47d87a37b6
rebat <rbdikmen@gmail.com>
2025-03-14 21:45:05 +0300

    Initialize plan-service with Spring Boot application and Maven wrapper

---
+++ plan-service/src/main/java/com/turkcell/crmmicroserviceshw4/planservice/PlanServiceApplication.java
@@ -0,0 +1,13 @@
+package com.turkcell.crmmicroserviceshw4.planservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlanServiceApplication.class, args);
+    }
+
+}
