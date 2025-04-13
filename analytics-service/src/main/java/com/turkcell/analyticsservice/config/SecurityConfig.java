f90b029b15074156c7903cad83d4bfac022e1337
rebat <rbdikmen@gmail.com>
2025-04-13 21:40:11 +0300

    Add security configuration and Feign client interceptor for JWT token handling

--- analytics-service/src/main/java/com/turkcell/analyticsservice/config/SecurityConfig.java
+++ analytics-service/src/main/java/com/turkcell/analyticsservice/config/SecurityConfig.java
@@ -1,19 +1,17 @@
-package com.turkcell.crmmicroservicesfinalproject.contractservice.config;
+package com.turkcell.analyticsservice.config;
 
+import feign.RequestInterceptor;
 import io.github.bothuany.security.config.BaseSecurityService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
-import feign.RequestInterceptor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
-import jakarta.servlet.http.HttpServletRequest;
-
-import lombok.RequiredArgsConstructor;
 
 @Configuration
 @RequiredArgsConstructor
